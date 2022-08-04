27d3b85d619fc61f487d6aef57106acf1103aead
rajratnamaitry <rajratnamaitry@gmail.com>
2022-08-04 22:46:19 +0530

    v2 first commit

--- src/defaults.ts
+++
@@ -1,8 +0,0 @@
-export const angularConfigFileNames: string[] = [
-    'angular.json',
-    '.angular.json',
-    'angular-cli.json',
-    '.angular-cli.json',
-];
-
-export const extensionName = 'Run Project';
